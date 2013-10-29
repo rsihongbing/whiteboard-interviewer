ed219bb3dc9199f34394e82823de0c7acd77b677
Yosan Namara <yosan_namara@hotmail.com>
2013-10-29 15:46:47 -0700

    Added class that generates unique password

---
+++ api/Utils/PasswordGenerator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * A Static class to generate password.
+ * @author ynamara
+ */
+class PasswordGenerator {
+	/** All of the chars in the generated password will be a subset of this. */
+	private static $RANGE = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+	
+	/** Ensures non-instantiability. */
+	private function __construct() {
+		throw new Exception("PasswordGenerator is a static class");
+	}
+	
+	/**
+	 * Generates random alphanumeric password with the specified length.
+	 * @param number $pwdLen
+	 * 	the desired length of the generated password: the default is 30. The behavior is undefined
+	 * 	when $pwdLen < 0.
+	 * @return string
+	 * 	random alphanumeric string with the specified length.
+	 */
+	public static function generatePassword($pwdLen = 30) {
+		$result = array();
+		$len = strlen(static::$RANGE) - 1;
+		for ($i = 0; $i < $pwdLen; $i++) {
+			$n = rand(0, $len);
+			$result[] = static::$RANGE[$n];
+		}
+		return implode($result);
+	}
+}
+?>
