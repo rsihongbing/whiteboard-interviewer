f0f94fc5ec305d25ba1da610a5e3f29482457a91
Christopher Tjong <ctjong@cubist.cs.washington.edu>
2013-10-30 16:07:17 -0700

    remove non-code related files

--- files/user.sql
+++
@@ -1,7 +0,0 @@
---
--- Create user
--- Since we are testing our project in cubist,
--- we are using this credential given by cubist administrator
---
-
-CREATE USER 'dannych'@'localhost' IDENTIFIED BY 'U6dPvb2m';
