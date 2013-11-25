7083fba28359b43975140d819fbdfd25f90c76b2
tuanvo1 <tuanvo@ougl-21-a.eplt.washington.edu>
2013-11-25 13:23:55 -0800

    FAQ section, AJAX loaded from json file

---
+++ js/faq.js
@@ -0,0 +1,24 @@
+
+/* AJAX loads json data for FAQ into mainpage */
+
+$(document).ready(function() {
+	console.log("Loaded JS Component: faq.js");
+	console.log("\tThis component will fail to load in chrome local file access, will be fine on HTTP server");
+	var faqURL = "docs/faq.json";
+	// DOM selector for container to inject FAQ into
+	var faqDOM = "#faq";
+
+	$.getJSON(faqURL, function (data) {
+		console.log("\t faq.js: in $.getJSON");
+		
+		 var items = [];
+		 var list = data.faq;
+		$.each(list, function(key, element ) {
+				items.push( "<blockquote><p>" + element.q + "</p><small> " + element.a + "</small></blockquote>" );
+		});
+		$( "<div/>", {
+			html: items.join( "" )
+		}).appendTo(faqDOM);
+
+	});
+});
