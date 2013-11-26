8d82d3e4d764e92bcf741dcb76033f9b0e25710c
Roi Sihong Bing <bing04@semanti.cs.washington.edu>
2013-11-26 11:11:37 -0800

    allow language mode changes to be reflected across all users

--- editor/editor.js
+++ editor/editor.js
@@ -10,12 +10,23 @@ function initEditor(languageMode, languageMime, containerId){
   });
 }
 
+function initLangRef() {
+  var room = location.search.split('&')[0];
+  var langRef = new Firebase('https://whiteboard-interviewer.firebaseIO.com/editor/' + room + '/language');
+  langRef.on('value', function(snapshot) {
+    var language = snapshot.val();
+    initEditor(language.langMode, language.langMime, "editor-container");
+    focusSelect(language.langMime);
+  });
+  langRef.set({langMode: "clike", langMime: "text/x-java"});
+  return langRef;
+}
+
 function langChanged(){
   var langId = $("#languages").val();
   var langMode = CodeMirror.modeInfo[langId].mode;
   var langMime = CodeMirror.modeInfo[langId].mime;
-  initEditor(langMode, langMime, "editor-container");
-  focusSelect(langMime);
+  langRef.set({langMime: langMime, langMode: langMode});
 }
 
 function initSelect(){
@@ -36,6 +47,8 @@ CodeMirror.modeInfo.sort(function(a,b){
    var bName = b.name.toLowerCase(); 
    return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
 });
+
+var langRef = initLangRef();
 initSelect();
 focusSelect("text/x-java");
 initEditor("clike", "text/x-java", "editor-container" );
