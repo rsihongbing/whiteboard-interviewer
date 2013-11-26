d0cf962b596ff0e69f24ab998e1aa049b65c8c97
dannych <dnny.ch@gmail.com>
2013-11-25 19:48:30 -0800

    add comment helper on error at form

--- js/Form.js
+++ js/Form.js
@@ -52,6 +52,19 @@ function InterviewForm() {
 	}
 
 	/**
+	 * Initialize time just like the begining
+	 */
+	this.initializeTime = function() {
+		if( !$_time.is(':disabled') ) {
+			$_time
+				.val('')
+				.parent().normal();
+
+			$('#interviewTimeHelp').html('');
+		}
+	}
+
+	/**
 	 * Reset the value of user input
 	 *
 	 * Empty all the field input,
@@ -63,23 +76,44 @@ function InterviewForm() {
 		$_intervieweeEmail
 			.val('')
 			.normal();
+		$('#intervieweeHelp').html('');
+
 		$_interviewerEmail
 			.val('')
 			.normal();
+		$('#interviewerHelp').html('');
+
 		$_date
 			.val('')
-			.normal();
-		
+			.parent().normal();
+		$('#interviewDateHelp').html('');
+
 		_i.initializeTime();
 	}
 
-	this.initializeTime = function() {
-		if( !$_time.is(':disabled') ) {
-			$_time
-				.val('')
-				.normal();
-		}
+	/**
+	 * Create a state of the form just like the beginning 
+	 *
+	 * executed when "New Session" button is clicked
+	 *
+	 * reinitialize the button states
+	 * and reset to blank form
+	 */
+	this.reinitialize = function() {
+		$("#resultloading").show('fast');
+		$("#formarea").show('slow');
+		$("#resultarea").hide();
+		$("#resultmessage").hide('slow');
+		$("#createBtn").removeAttr("disabled");
+		$("#resetBtn").show().removeAttr("disabled");
+		
+		$("#newBtn").hide();
+		
+		if ($("#newBtn").val() === "true")
+			_i.initialize();
 	}
+
+	
 
 	/**
 	 * Generate today's date and the current time
@@ -130,7 +164,8 @@ function InterviewForm() {
 
 		// date must be specified
 		if (date === null || date === "") {
-			$_date.mark(false);
+			$_date.parent().mark(false);
+			$('#interviewDateHelp').html('Please submit your interview date');
 			return false;
 		}
 
@@ -139,7 +174,8 @@ function InterviewForm() {
 		// check the 'first' validity of the input
 		// must match to the regex
 		if (yearMonthDay === null) {
-			$_date.mark(false);
+			$('#interviewDateHelp').html('Please submit a valid date');
+			$_date.parent().mark(false);
 			return false;
 		}
 
@@ -160,7 +196,13 @@ function InterviewForm() {
 			check = true;
 		}
 
-		$_date.mark(check);
+		if (check) {
+			$('#interviewDateHelp').html('');
+		} else {
+			$('#interviewDateHelp').html('Your interview date has passed');
+		}
+
+		$_date.parent().mark(check);
 
 		return check;
 	}
@@ -195,6 +237,7 @@ function InterviewForm() {
 		var time = $_time.val();
 
 		if (time === 'Anytime') {
+			$('#interviewTimeHelp').html('');
 			return true;
 		}
 
@@ -204,15 +247,17 @@ function InterviewForm() {
 
 		// we should know the day first to verify the time
 		if ( yearMonthDay == null ) {
-			$_date.normal();
-			$_date.mark(false);
+			$_time.normal();
+			$_date.parent().mark(false);
+			$('#interviewDateHelp').html('Please input a valid date first');
 			return false;
 		}
 
 		// user must input the time 
 		// when not choosing 'Anytime' 
 		if ( hourMinute == null ) {
-			$_time.mark(false);
+			$_time.parent().mark(false);
+			$('#interviewTimeHelp').html('Please input a valid time');
 			return false;
 		} 
 		
@@ -223,15 +268,16 @@ function InterviewForm() {
 													yearMonthDay[3],
 													hourMinute[1],
 													hourMinute[2]);
-		var check = false;
 
-		if (input.getTime() >= now.getTime()) {
-			check = true;
+		if (input.getTime() < now.getTime()) {
+			$_time.parent().mark(false);
+			$('#interviewTimeHelp').html('Your interview time has already passed');
+			return false;
 		}
 		
-		$_time.mark(check);
-
-		return check;
+		$_time.parent().mark(true);
+		$('#interviewTimeHelp').html('');
+		return true;
 	}
 
 	/**
@@ -276,13 +322,30 @@ function InterviewForm() {
 	 */
 	this.checkInterviewerEmail = function() {
 		var email = $_interviewerEmail.val();
-	 	var check = checkEmail(email);
 
-	 	if (email === $_intervieweeEmail.val() ) {
-	 		check = false;
+		if (email === "") {
+			$_interviewerEmail.mark(false);
+	 		$('#interviewerHelp').html("Please submit the interviewer's email");
+	 		return false;
+		}
+
+	 	if (!checkEmail(email)) {
+	 		// not valid syntax
+	 		$_interviewerEmail.mark(false);
+	 		$('#interviewerHelp').html("Invalid email");
+	 		return false;
 	 	}
 
-	 	$_interviewerEmail.mark(check);
+	 	if (email === $_intervieweeEmail.val() ) {
+	 		// same email as the other one
+	 		$_interviewerEmail.mark(false);
+	 		$('#interviewerHelp').html("This email cannot be the same as interviewee email");
+	 		return false;
+	 	}
+
+	 	// pass all checks
+	 	$_interviewerEmail.mark(true);
+	 	$('#interviewerHelp').html("");
 
 	 	return check;
 	}
@@ -296,37 +359,32 @@ function InterviewForm() {
 	 */
 	this.checkIntervieweeEmail = function() {
 		var email = $_intervieweeEmail.val();
-		var check = checkEmail(email);
+
+		if (email === "") {
+			$_intervieweeEmail.mark(false);
+	 		$('#intervieweeHelp').html("Please submit the interviewer's email");
+	 		return false;
+		}
+
+		if (!checkEmail(email)) {
+			// invalid syntax
+			$_intervieweeEmail.mark(false);
+			$('#intervieweeHelp').html("Invalid email");
+			return false;
+		}
 
 		if (email === $_interviewerEmail.val() ) {
-	 		check = false;
+			// same email
+			$_intervieweeEmail.mark(false);
+			$('#intervieweeHelp').html("This email cannot be the same as interviewer's email");
+	 		return false;
 	 	}
 
-		$_intervieweeEmail.mark(check);
+	 	// pass all checks
+		$_intervieweeEmail.mark(true);
+		$('#intervieweeHelp').html("");
 
-		return check;
-	}
-
-	/**
-	 * Create a state of the form just like the beginning 
-	 *
-	 * executed when "New Session" button is clicked
-	 *
-	 * reinitialize the button states
-	 * and reset to blank form
-	 */
-	this.reinitialize = function() {
-		$("#resultloading").show('fast');
-		$("#formarea").show('slow');
-		$("#resultarea").hide();
-		$("#resultmessage").hide('slow');
-		$("#createBtn").removeAttr("disabled");
-		$("#resetBtn").show().removeAttr("disabled");
-		
-		$("#newBtn").hide();
-		
-		if ($("#newBtn").val() === "true")
-			_i.initialize();
+		return true;
 	}
 
 	/**
