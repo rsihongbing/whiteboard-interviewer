130c3c0437f71c857a619e7863fea427c8ffd161
Christopher Tjong <ctjong@cubist.cs.washington.edu>
2013-11-25 16:20:06 -0800

    modify menu bar on interview interface

--- room.php
+++ room.php
@@ -11,14 +11,15 @@ if($x == 0)  header( 'Location: 404.html' );
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Whiteboard Interviewer</title>
+	
+        <title>Whiteboard Interviewer</title>
 
-   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <!-- jQuery -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         
-   		<!-- Bootstrap -->
+   	<!-- Bootstrap -->
     	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     	<link rel="stylesheet" href="css/supersized.core.css" media="screen"/>
 
@@ -30,20 +31,19 @@ if($x == 0)  header( 'Location: 404.html' );
         <script id="langScript" src="editor/codemirror/mode/clike/clike.js"></script>
         <link rel="stylesheet" href="editor/codemirror/lib/codemirror.css" />
         <link rel="stylesheet" href="editor/firepad.css" />
-        <!--<script src="editor/userList.js"></script>  -->
-        
+                
         <!-- Video -->
-		<script src="http://simplewebrtc.com/latest.js"></script>
-		<script src="js/video.js"></script>
+	<script src="http://simplewebrtc.com/latest.js"></script>
+	<script src="js/video.js"></script>
     	
         <style>
         .navbar { margin-top: 10px; }
         .navbar button { margin: 8px 6px 8px 2px; }
         .video{ background-color:black; }
-		#leftMenu {	background-color: #eeeeee; }
-  		#textEditor { font-family: courier; border:0; height:600px; }
-.cols{float:left; position:relative;}
-#remote video{width:100%;}
+	#leftMenu {	background-color: #eeeeee; }
+        #textEditor { font-family: courier; border:0; height:600px; }
+        .cols{float:left; position:relative;}
+        #remote video{width:100%;}
     	</style>
         
 	</head>
@@ -51,39 +51,28 @@ if($x == 0)  header( 'Location: 404.html' );
 	<body>
 		<div class="container">
 			<nav class="navbar navbar-default navbar-inverse" role="navigation">
-            
-			  <!-- Brand and toggle get grouped for better mobile display -->
-			  <div class="navbar-header">
-			    <a class="navbar-brand" href="index.html">Whiteboard Interviewer</a>
-			  </div>
-
-			  <!-- Collect the nav links, forms, and other content for toggling -->
-			  <div class="collapse navbar-collapse navbar-ex1-collapse">
-			    <ul class="nav navbar-nav">
-			      <li class="active"><a href="#">Intro</a></li>
-			      <li><a href="#">Team</a></li>
-			      <li><a href="#">Contact</a></li>
-			    </ul>
-			  </div><!-- /.navbar-collapse -->
-			</nav>
+      			    <div class="navbar-header">
+			        <a class="navbar-brand" href="index.html" style="margin: 0 auto;">Whiteboard Interviewer</a>
+			    </div>
+  			</nav>
 
 			<!-- put content here -->
-		    <div style="background-color:rgba(255,255,255,0.75); padding:50px; border-radius:25px; overflow:hidden; height:550px; width:1150px;">
+	                <div style="background-color:rgba(255,255,255,0.75); padding:50px; border-radius:25px; overflow:hidden; height:550px; width:1150px;">
 
-  <div class="row" style="position:relative;">
-					<div style="position:absolute; top:30px; left:0; height:400px; width:300px; overflow:hidden;">
-                            <div id="remote" class="video" style="position:absolute; top:0; left:0; width:100%; height:100%;"></div>
-                            <video id="local" class="video" style="position:absolute; top:250px; left:100px; width:100px; height: 100px; z-index:2;"></video>
-                        </div>
+                            <div class="row" style="position:relative;">
+			        <div style="position:absolute; top:30px; left:0; height:400px; width:300px; overflow:hidden;">
+                                    <div id="remote" class="video" style="position:absolute; top:0; left:0; width:100%; height:100%;"></div>
+                                    <video id="local" class="video" style="position:absolute; top:250px; left:100px; width:100px; height: 100px; z-index:2;"></video>
+                                </div>
 
-					<div style="position:absolute; top:0; left:350px; z-index:3; width:700px;">
-                        <div style="margin:5px; text-align:center;">Language Mode : <select id='languages'></select></div>
-                        <div id="editor-container"></div>
-                    </div>
+                      		<div style="position:absolute; top:0; left:350px; z-index:3; width:700px;">
+                                    <div style="margin:5px; text-align:center;">Language Mode : <select id='languages'></select></div>
+                                    <div id="editor-container"></div>
+                                </div>
 
-				</div>
+		             </div>
                 
-			</div> <!-- /container -->
+		         </div> <!-- /container -->
 
 		</div><!-- /container -->
 		
@@ -92,9 +81,8 @@ if($x == 0)  header( 'Location: 404.html' );
 		<script src="js/bootstrap.min.js"></script>
 		<!-- library for full screen background -->
 		<script src="js/supersized.min.js" type="text/javascript"></script>
-		<!-- Loads full screen background" -->
+		<!-- Loads full screen background -->
 		<script src="js/loadFullSizeBG.js" type="text/javascript"></script>
-		<script src="js/createSession.js"></script>
 		<script src="editor/editor.js"></script>
         
 	</body>
